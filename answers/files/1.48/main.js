37f99b1d332e8b30d894013fdb7f06f2c6bad763
Evgeniy Semenenko <evgeniy.s@2muchcoffee.com>
2021-12-13 16:53:36 +0200

    1 - 44.1 Answer

--- answers/files/1.48/main.js
+++ answers/files/1.48/main.js
@@ -47,7 +47,7 @@ function addEvents () {
     setExpression('');
     setLeftOperand('');
     setRightOperand('');
-    // setOperator('');
+    //setOperator('');
     setOperator(ADDITION);
     setResult('');
 
--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -1,41 +1,47 @@
+let leftOperand;
+let rightOperand;
+let clear;
+let result;
 function onLeftOperandChange(value) {
-  console.log('onLeftOperandChange', value)
+  console.log('onLeftOperandChange', value);
+  leftOperand = value;
 };
-
+console.log(leftOperand);
 function onRightOperandChange(value) {
-  console.log('onRightOperandChange', value)
+  console.log('onRightOperandChange', value);
+  rightOperand = value;
 };
 
 function onOperatorChange(value) {
   console.log('onOperatorChange', value)
 };
-// let a = onLeftOperandChange();
-// let b = onRightOperandChange();
-// console.log(a);
-// console.log(b);
-// function onSubmit () {
+
+function onSubmit () {
 //     switch (onOperatorChange()) {
 //       case 'ADDITION' :
-//         return Number(onLeftOperandChange(value)) + Number(onRightOperandChange(value));
+//         return result = (Number(onLeftOperandChange(value)) + Number(onRightOperandChange(value)));
 //       case 'SUBTRACTION' :
-//         return Number(onLeftOperandChange(value)) - Number(onRightOperandChange(value));
+//         return result = Number(onLeftOperandChange(value)) - Number(onRightOperandChange(value));
 //       case 'MULTIPLICATION' :
-//         return Number(onLeftOperandChange(value)) * Number(onRightOperandChange(value));
+//         return result = Number(onLeftOperandChange(value)) * Number(onRightOperandChange(value));
 //       case 'EXPONENTIATION' :
-//         return Number(onLeftOperandChange(value)) ** Number(onRightOperandChange(value));
+//         return result = Number(onLeftOperandChange(value)) ** Number(onRightOperandChange(value));
 //       case 'DIVISION' :
-//         return Number(onLeftOperandChange(value)) / Number(onRightOperandChange(value));
+//         return result = Number(onLeftOperandChange(value)) / Number(onRightOperandChange(value));
 //       case 'MODULUS' :
-//         return Number(onLeftOperandChange(value)) % Number(onRightOperandChange(value));
+//         return result = Number(onLeftOperandChange(value)) % Number(onRightOperandChange(value));
 //       case 'ROOT' :
-//         return Number(onLeftOperandChange(value)) ** 1/Number(onRightOperandChange(value));
+//         return result = Number(onLeftOperandChange(value)) ** 1/Number(onRightOperandChange(value));
 //     }
 //   }
 //   console.log(Number(onLeftOperandChange(value)))
 //   console.log(onSubmit())
-// }
+ }
 
 function onClear () {
   console.log('onClear')
+  clear = onClear();
 }
+
 // setExpression('(2 + 3) * 4')
+// setResult(50);
