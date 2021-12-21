7a4b6c9f901626e4e5e35e4af691b8fbd2464418
Evgeniy Semenenko <evgeniy.s@2muchcoffee.com>
2021-12-21 13:31:05 +0200

    1 - 43.3 Answer

--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -6,76 +6,79 @@ let resultString;
 let someString;
 
 function getResult(leftOperand, operatorName, rightOperand) {
-  switch (operatorName) {
-    case 'ADDITION':
-      return (leftOperand + rightOperand);
-    case 'SUBTRACTION':
-      return (leftOperand - rightOperand);
-    case 'MULTIPLICATION':
-      return (leftOperand * rightOperand);
-    case 'EXPONENTIATION':
-      return (leftOperand ** rightOperand);
-    case 'DIVISION':
-      return (leftOperand / rightOperand);
-    case 'MODULUS':
-      return (leftOperand % rightOperand);
-    case 'ROOT':
-      return (leftOperand ** (1/rightOperand));
-  };
+    switch (operatorName) {
+        case 'ADDITION':
+            return (leftOperand + rightOperand);
+        case 'SUBTRACTION':
+            return (leftOperand - rightOperand);
+        case 'MULTIPLICATION':
+            return (leftOperand * rightOperand);
+        case 'EXPONENTIATION':
+            return (leftOperand ** rightOperand);
+        case 'DIVISION':
+            return (leftOperand / rightOperand);
+        case 'MODULUS':
+            return (leftOperand % rightOperand);
+        case 'ROOT':
+            return (leftOperand ** (1 / rightOperand));
+    }
+    ;
 };
 
 function onLeftOperandChange(value) {
-  leftOperand = +value;
+    leftOperand = +value;
 }
 
 function onRightOperandChange(value) {
-  rightOperand = +value;
+    rightOperand = +value;
 }
 
 function onOperatorChange(value) {
-  operatorName = value;
-switch (operatorName) {
-  case 'ADDITION' :
-    operatorSymbol = ' + '
-    break;
-  case 'SUBTRACTION' :
-    operatorSymbol = ' - '
-    break;
-  case 'MULTIPLICATION' :
-    operatorSymbol = ' * '
-    break;
-  case 'EXPONENTIATION' :
-    operatorSymbol = ' xª '
-    break;
-  case 'DIVISION' :
-    operatorSymbol = ' / '
-    break;
-  case 'MODULUS' :
-    operatorSymbol = ' % '
-    break;
-  case 'ROOT' :
-    operatorSymbol = ' √ '
-    break;
-}
+    operatorName = value;
+    switch (operatorName) {
+        case 'ADDITION' :
+            operatorSymbol = ' + '
+            break;
+        case 'SUBTRACTION' :
+            operatorSymbol = ' - '
+            break;
+        case 'MULTIPLICATION' :
+            operatorSymbol = ' * '
+            break;
+        case 'EXPONENTIATION' :
+            operatorSymbol = ' xª '
+            break;
+        case 'DIVISION' :
+            operatorSymbol = ' / '
+            break;
+        case 'MODULUS' :
+            operatorSymbol = ' % '
+            break;
+        case 'ROOT' :
+            operatorSymbol = ' √ '
+            break;
+    }
 }
 
-function onSubmit () {
-  if (leftOperand && operatorSymbol && rightOperand) {
-    if (!someString) {
-      someString = leftOperand + operatorSymbol + rightOperand;
-    } else {
-      someString = '( '+ `${someString}` + ')' + `${operatorSymbol}` + `${rightOperand}`;
-    };
-    resultString = someString;
-    setExpression(resultString);
-    setResult(getResult(leftOperand, operatorName, rightOperand));
-  };
+function onSubmit() {
+    if (leftOperand && operatorSymbol && rightOperand) {
+        if (!someString) {
+            someString = leftOperand + operatorSymbol + rightOperand;
+        } else {
+            someString = '( ' + `${someString}` + ')' + `${operatorSymbol}` + `${rightOperand}`;
+        }
+        ;
+        resultString = someString;
+        setExpression(resultString);
+        setResult(getResult(leftOperand, operatorName, rightOperand));
+    }
+    ;
 
-console.log('onSubmit');
+    console.log('onSubmit');
 };
 
-function onClear () {
-  someString = undefined;
-  setResult(undefined);
-  setExpression('');
+function onClear() {
+    someString = undefined;
+    setResult(undefined);
+    setExpression('');
 };
