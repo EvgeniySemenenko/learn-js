335bef7abfbdd0f99c248167cdc1624fe2648f0b
Evgeniy Semenenko <evgeniy.s@2muchcoffee.com>
2022-01-13 11:21:21 +0200

    2 - 22 Answer

--- answers/files/1.1/script.js
+++ answers/files/1.1/script.js
@@ -1,1 +1,12 @@
-alert("Hello, World!");
+
+function User () {
+     user = {
+     name: 'John',
+     user() {
+       return this.name;
+    }
+}
+
+const user = new User();
+let name = user.user().user().user().user().user().user().name;
+console.log(name);
